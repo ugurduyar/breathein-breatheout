2bd50ebc7d122610a6abca6f28498c93b1466450
ugurduyar <ismailugurduyar@gmail.com>
2020-06-09 15:04:51 +0300

    time variables added- text removed

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breathAnimation() {
+  console.log("");
+}
